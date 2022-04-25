c0cc3cc3510b6ab125ad6d07afe28599a7420c79
Arnau-webDev <arnauma16@gmail.com>
2022-04-25 12:22:19 +0200

    Changed request max size due to error when uploading image to post

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 import express from "express";
-// import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from "./routes/posts.js";
@@ -9,8 +8,7 @@ import dotenv from "dotenv";
 const app = express();
 dotenv.config();
 
-
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
