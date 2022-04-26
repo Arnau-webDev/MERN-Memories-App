005d93d8fe900c2b2f81dad0a22be74ceb472709
Arnau-webDev <arnauma16@gmail.com>
2022-04-26 14:18:10 +0200

    Added backend functionality to enable comments

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getPosts, getPost, createPost, updatePost, deletePost, likePost, getPostsBySearch } from '../controllers/posts.js';
+import { getPosts, getPost, createPost, updatePost, deletePost, likePost, getPostsBySearch, commentPost } from '../controllers/posts.js';
 import auth from '../middleware/auth.js';
 
 const router = express.Router();
@@ -14,5 +14,6 @@ router.post("/", createPost);
 router.patch("/:id", updatePost);
 router.delete("/:id", deletePost);
 router.patch("/:id/likePost", likePost);
+router.post("/:id/commentPost", commentPost);
 
 export default router;
