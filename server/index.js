0b42d37a15524d7a227cef272cc9f575d04b4675
Arnau-webDev <arnauma16@gmail.com>
2022-04-27 14:38:37 +0200

    Changed API for backEnd from localhost to production endpoint

--- server/index.js
+++ server/index.js
@@ -16,9 +16,10 @@ app.use("/user", userRoutes);
 
 app.get("/", (req, res) => res.send("Welcome to memories API"));
 
+const CONNECTION_URL = "mongodb+srv://arnauMQ:arnauMQ1234@cluster0.kwzzr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((err) => console.log(err.message));
 
