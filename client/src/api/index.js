56a6e67c91905b5730931479af0b42e3571c69be
Arnau-webDev <arnauma16@gmail.com>
2022-11-21 13:32:16 +0100

    changed service endpoint from heroku to render

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const API = axios.create({ baseURL: "http://localhost:5000" }); LOCALHOST SERVER
-const API = axios.create({ baseURL: "https://mernxperiences2.herokuapp.com/" });
+const API = axios.create({ baseURL: "https://memories-service.onrender.com/" });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem("profile")) {
